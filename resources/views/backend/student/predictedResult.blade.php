3ec74e71fc80547727dd18cdbfe422d9515686f4
islamMaruf <marufislam7424@gmail.com>
2018-12-07 23:23:38 +0600

    student result prediction completed

--- resources/views/backend/student/predictedResult.blade.php
+++ resources/views/backend/student/predictedResult.blade.php
@@ -95,112 +95,105 @@
     <script>
         var results = [];
         results = @json($studentMark);
-            var attendance = results[0].attendance;
-            var assignment = results[0].assignment;
-            var quiz = results[0].quiz;
-            var midterm = results[0].midTerm;
-            var presentation = results[0].presentation;
+        var attendance = results[0].attendance;
+        var assignment = results[0].assignment;
+        var quiz = results[0].quiz;
+        var midterm = results[0].midTerm;
+        var presentation = results[0].presentation;
+        function checkUpper27() {
+            var divclass = document.getElementById('finalFirst');
+            divclass.hidden = true;
+            if(midterm > 13.5 ){
+                var secondDiv = document.getElementById('finalFourth');
+                secondDiv.hidden = false;
 
-            function checkUpper27() {
-                var divclass = document.getElementById('finalFirst');
-                divclass.hidden = true;
-                if(midterm > 13.5 ){
-                    var secondDiv = document.getElementById('finalFourth');
-                    secondDiv.hidden = false;
 
-
-                }else if(midterm<=13.5){
-                    if (midterm>6){
-                        if(quiz>10.67){
-                            result('resultId','btn-primary','Good');
-                        }else if (quiz<=10.67){
-                            if(attendance>6.13){
-                                if(midterm>10){
-                                    result('resultId','btn-primary','Good');
-                                }else if(midterm<=10){
-                                    result('resultId','btn-info','Average');
-                                }
-
-                            }else if (attendance<=6.13){
+            }else if(midterm<=13.5){
+                if (midterm>6){
+                    if(quiz>10.67){
+                        result('resultId','btn-primary','Good');
+                    }else if (quiz<=10.67){
+                        if(attendance>6.13){
+                            if(midterm>10){
+                                result('resultId','btn-primary','Good');
+                            }else if(midterm<=10){
                                 result('resultId','btn-info','Average');
                             }
-                        }
 
-                    } else if(midterm<=6){
-                        var divclss = document.getElementById('finalThird');
-                        divclss.hidden = true;
+                        }else if (attendance<=6.13){
+                            result('resultId','btn-info','Average');
+                        }
                     }
 
+                } else if(midterm<=6){
+                    var divclss = document.getElementById('finalThird');
+                    divclss.hidden = true;
                 }
 
-
             }
 
-            function  checkLower27(){
-                var divclas = document.getElementById('finalFirst');
-                divclas.hidden = true;
-                var divShow = document.getElementById('finalSecond');
-                divShow.hidden = false;
 
-            }
+        }
+        function  checkLower27(){
+            var divclas = document.getElementById('finalFirst');
+            divclas.hidden = true;
+            var divShow = document.getElementById('finalSecond');
+            divShow.hidden = false;
 
-            function checkUpper31() {
-                result('resultId','btn-info','Average');
+        }
+        function checkUpper31() {
+            result('resultId','btn-info','Average');
+        }
+        function checkLower31() {
+            result('resultId','btn-warning','Below Average');
+        }
+        function checkUpper33() {
+            result('resultId','btn-success','Excellent');
+        }
+        function checkLower33() {
+            if(midterm>20){
+                result('resultId','btn-success','Excellent');
+            }else if (midterm<=20){
+                result('resultId','btn-primary','Good');
             }
-            function checkLower31() {
+        }
+        function checkUpper8() {
+            if(quiz>8){
+                if(midterm>12.5){
+                    if (presentation>6.2){
+                        result('resultId','btn-primary','Good');
+                    }else if(presentation<=6.2){
+                        result('resultId','btn-info','Average');
+                    }
+
+                }else if(midterm<=12.5){
+                    if(attendance>6.68){
+                        result('resultId','btn-primary','Good');
+                    }else if(attendance<=6.68){
+                        if (assignment>3.5){
+                            result('resultId','btn-info','Average');
+                        } else  if(assignment<=3.5){
+                            result('resultId','btn-warning','Below Average');
+                        }
+
+                    }
+                }
+
+            }else if(quiz<=8){
                 result('resultId','btn-warning','Below Average');
             }
 
-            function checkUpper33() {
-                result('resultId','btn-success','Excellent');
-            }
-            function checkLower33() {
-                if(midterm>20){
-                    result('resultId','btn-success','Excellent');
-                }else if (midterm<=20){
-                    result('resultId','btn-primary','Good');
-                }
-            }
 
-            function checkUpper8() {
-                if(quiz>8){
-                    if(midterm>12.5){
-                        if (presentation>6.2){
-                            result('resultId','btn-primary','Good');
-                        }else if(presentation<=6.2){
-                            result('resultId','btn-info','Average');
-                        }
+        }
+        function checkLower8() {
+            result('resultId','btn-danger','Fail')
+        }
+        function result(classId,className,status) {
+            var results = document.getElementById(classId);
+            results.classList.add(className);
+            results.innerHTML = status;
+        }
 
-                    }else if(midterm<=12.5){
-                        if(attendance>6.68){
-                            result('resultId','btn-primary','Good');
-                        }else if(attendance<=6.68){
-                            if (assignment>3.5){
-                                result('resultId','btn-info','Average');
-                            } else  if(assignment<=3.5){
-                                result('resultId','btn-warning','Below Average');
-                            }
-
-                        }
-                    }
-
-                }else if(quiz<=8){
-                    result('resultId','btn-warning','Below Average');
-                }
-
-
-            }
-
-            function checkLower8() {
-                result('resultId','btn-danger','Fail')
-            }
-
-
-            function result(classId,className,status) {
-                var results = document.getElementById(classId);
-                results.classList.add(className);
-                results.innerHTML = status;
-            }
 
 
 
